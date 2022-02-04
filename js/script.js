de290c5d532dd653e21676e4fb7cc526b2704168
Meg <92758663+mbarnstien@users.noreply.github.com>
2022-02-04 12:01:57 -0800

    Added guess input criteria and messages

--- js/script.js
+++ js/script.js
@@ -6,7 +6,9 @@ const remainingGuesses = document.querySelector(".remaining");
 const spanGuesses = document.querySelector(".remaining span");
 const messages = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
+
 const word = "magnolia";
+const guessedLetters = [];
 
 const circle = function (word) {
     const circleSymbol = [];
@@ -21,7 +23,37 @@ circle(word);
 
 guessButton.addEventListener("click", function (e) {
     e.preventDefault();
-    const guessedLetter = inputLetter.value;
-    console.log(guessedLetter);
+    const guess = inputLetter.value;
+    console.log(guess);
     inputLetter.value = "";
+    messages.innerText = "";
+    
+    const correctGuess = validateInput(guess);
+
+    if (correctGuess) {
+        makeGuess(guess);
+    }
 });
+
+const validateInput = function (input) {
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input.length === 0) {
+        messages.innerText = `Please enter a letter`;
+    } else if (input.length > 1) {
+        messages.innerText = `Please enter only 1 letter at a time`;
+    } else if (!input.match(acceptedLetter)) {
+        messages.innerText = `Symbols or numbers are not a part of this word!`;
+    } else {
+        return input;
+    }
+};
+
+const makeGuess = function (guess) {
+    guess = guess.toUpperCase();
+    if (guessedLetters.includes(guess)) {
+        messages.innerText = `You already guessed that! Try again.`;
+    } else {
+        guessedLetters.push(guess);
+        console.log(guessedLetters);
+    }
+};
